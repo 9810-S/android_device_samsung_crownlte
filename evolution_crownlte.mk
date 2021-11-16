061f85ddf4d774443c00093b15465ccb0cb25971
Adrian Sian <adriansian@gmail.com>
2021-11-16 17:45:43 +0800

    crownlte: remove pixel charger

--- evolution_crownlte.mk
+++ evolution_crownlte.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
 
